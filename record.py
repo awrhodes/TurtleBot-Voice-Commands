f61034067d2c2fa22dbae72a47279179e0683949
sujaymanb <sujaymanb@gmail.com>
2017-02-25 21:47:03 -0500

    voice recorder

---
+++ record.py
@@ -0,0 +1,16 @@
+import sounddevice as sd
+import numpy as np
+import soundfile as sf
+#sd.default.samplerate = 44100
+
+duration = 5
+
+def callback(indata, frames, time, status):
+	volume_norm = np.linalg.norm(indata)*10
+	if volume_norm > 10:
+		recording = sd.rec(int(duration * 44100), samplerate=44100, channels=2, blocking=True)
+		sf.write('test.flac', recording, 44100, 'PCM_16')
+
+
+with sd.InputStream(callback=callback):
+	sd.sleep(duration * 1000)
