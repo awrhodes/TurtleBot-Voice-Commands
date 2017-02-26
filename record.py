11aa2afac1f3742e80619a87be412ca20b51e7cc
Alex <Alex Rhodes>
2017-02-26 01:04:30 -0500

    Fixed formatting

--- record.py
+++ record.py
@@ -7,10 +7,10 @@ duration = 5
 
 def callback(indata, frames, time, status):
 	volume_norm = np.linalg.norm(indata)*10
-	if volume_norm > 10:
-		recording = sd.rec(int(duration * 44100), samplerate=44100, channels=1, blocking=True)
-		sf.write('test.flac', recording, 44100, 'PCM_16')
-
+        if volume_norm > 85:
+            recording = sd.rec(int(duration * 44100),
+                               samplerate=44100, channels=1, blocking=True)
+            sf.write('test.flac', recording, 44100, 'PCM_16')
 
 with sd.InputStream(callback=callback):
 	sd.sleep(duration * 1000)
