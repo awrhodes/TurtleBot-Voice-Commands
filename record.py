5a4eea392764a2e1c3505c4066d8f5d2b271db59
Alex <Alex Rhodes>
2017-02-27 15:31:35 -0500

    Can now specify duration from command line.

--- record.py
+++ record.py
@@ -1,16 +1,29 @@
 import sounddevice as sd
 import numpy as np
 import soundfile as sf
-#sd.default.samplerate = 44100
+import sys
 
-duration = 5
+class Recorder:
+    'Microphone recording object.' #Documentation
 
-def callback(indata, frames, time, status):
-	volume_norm = np.linalg.norm(indata)*10
-        if volume_norm > 85:
-            recording = sd.rec(int(duration * 44100),
-                               samplerate=44100, channels=1, blocking=True)
-            sf.write('test.flac', recording, 44100, 'PCM_16')
+    def callback(indata, frames, time, status):
+            volume_norm = np.linalg.norm(indata)*10
+            if volume_norm > 85:
+                try:
+                    recording = sd.rec(int((int(sys.argv[1]) * 44100)),
+                                       samplerate=44100,channels=1,
+                                       blocking=True)
+                    sf.write(sys.argv[2],recording,44100,'PCM_16')
+                except IndexError:
+                    print("Line 24.")
+                    print("Error: Filename to save recording to not specified.")
+                except ValueError:
+                    print("Line 21.")
+                    print("Error: Duration passed not int.")
 
-with sd.InputStream(callback=callback):
-	sd.sleep(duration * 1000)
+    with sd.InputStream(callback=callback):
+        try:
+             sd.sleep(int(sys.argv[1]) * 1000)
+        except TypeError:
+            print("Line 34.")
+            print("Error: Duration passed not int.")
