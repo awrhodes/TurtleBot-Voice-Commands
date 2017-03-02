84a087eee61c007f359a703b31c44a6b0d1e3872
sujaymanb <sujaymanb@gmail.com>
2017-03-01 20:36:07 -0500

    Tab errors introduced

--- record.py
+++ record.py
@@ -9,41 +9,41 @@ filename = "command.flac"
 threshold = 85
 
 if sys.argv[1] != None:
-	duration = sys.argv[1]
+    duration = sys.argv[1]
 
 if sys.argv[2] != None:
-	filename = sys.argv[2]
+    filename = sys.argv[2]
 
 if sys.argv[3] != None:
-	threshold = sys.argv[3]
+    threshold = sys.argv[3]
 
 if sys.argv[4] == "Debug":
-	debug = True
+    debug = True
 
 print("Running with:\nduration:\t" + duration + 
 		"\nfilename:\t" + filename + "\nthreshold:\t" + threshold)
 
 def callback(indata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
-	if debug == True:
-		print("[debug] volume_norm: " + volume_norm)
-    
-	if volume_norm > threshold:
-        try:
-            recording = sd.rec(int((int(sys.argv[1]) * 44100)),
-                               samplerate=44100,channels=1,
-                               blocking=True)
-            sf.write(filename,recording,44100,'PCM_16')
-        except IndexError:
-            print("Line 24.")
-            print("Error: Filename to save recording to not specified.")
-        except ValueError:
-            print("Line 21.")
-            print("Error: Duration passed not int.")
+    if debug == True:
+        print("[debug] volume_norm: " + volume_norm)
+
+    if volume_norm > threshold:
+    try:
+        recording = sd.rec((int(duration * 44100),
+                           samplerate=44100,channels=1,
+                           blocking=True)
+        sf.write(filename,recording,44100,'PCM_16')
+    except IndexError:
+        print("Line 24.")
+        print("Error: Filename to save recording to not specified.")
+    except ValueError:
+        print("Line 21.")
+        print("Error: Duration passed not int.")
 
 with sd.InputStream(callback=callback):
     try:
-         sd.sleep(int(duration) * 1000)
+        sd.sleep(int(duration) * 1000)
     except TypeError:
         print("Line 34.")
         print("Error: Duration passed not int.")
