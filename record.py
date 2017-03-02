dffa776feb60559c11e88e40a835c18576bad56d
sujaymanb <sujaymanb@gmail.com>
2017-03-01 20:24:54 -0500

    Added argument/defaults

--- record.py
+++ record.py
@@ -3,27 +3,47 @@ import numpy as np
 import soundfile as sf
 import sys
 
-class Recorder:
-    'Microphone recording object.' #Documentation
+#default values for arguments
+duration = 5
+filename = "command.flac"
+threshold = 85
 
-    def callback(indata, frames, time, status):
-            volume_norm = np.linalg.norm(indata)*10
-            if volume_norm > 85:
-                try:
-                    recording = sd.rec(int((int(sys.argv[1]) * 44100)),
-                                       samplerate=44100,channels=1,
-                                       blocking=True)
-                    sf.write(sys.argv[2],recording,44100,'PCM_16')
-                except IndexError:
-                    print("Line 24.")
-                    print("Error: Filename to save recording to not specified.")
-                except ValueError:
-                    print("Line 21.")
-                    print("Error: Duration passed not int.")
+if sys.argv[1] != None:
+	duration = sys.argv[1]
 
-    with sd.InputStream(callback=callback):
+if sys.argv[2] != None:
+	filename = sys.argv[2]
+
+if sys.argv[3] != None:
+	threshold = sys.argv[3]
+
+if sys.argv[4] == "Debug":
+	debug = True
+
+print("Running with:\nduration:\t" + duration + 
+		"\nfilename:\t" + filename + "\nthreshold:\t" + threshold)
+
+def callback(indata, frames, time, status):
+    volume_norm = np.linalg.norm(indata)*10
+	if debug == True:
+		print("[debug] volume_norm: " + volume_norm)
+    
+	if volume_norm > threshold:
         try:
-             sd.sleep(int(sys.argv[1]) * 1000)
-        except TypeError:
-            print("Line 34.")
+            recording = sd.rec(int((int(sys.argv[1]) * 44100)),
+                               samplerate=44100,channels=1,
+                               blocking=True)
+            sf.write(filename,recording,44100,'PCM_16')
+        except IndexError:
+            print("Line 24.")
+            print("Error: Filename to save recording to not specified.")
+        except ValueError:
+            print("Line 21.")
             print("Error: Duration passed not int.")
+
+with sd.InputStream(callback=callback):
+    try:
+         sd.sleep(int(duration) * 1000)
+    except TypeError:
+        print("Line 34.")
+        print("Error: Duration passed not int.")
