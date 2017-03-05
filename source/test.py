e7d7fa2bdfc1c989ecf8e577bb170eb74b02c106
Alex <Alex Rhodes>
2017-03-04 19:32:22 -0500

    Won't work without custom sphinx infor.

--- source/test.py
+++ source/test.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
 import speech_recognition as sr
+from pocketsphinx.pocketsphinx import *
 
 recognizer = sr.Recognizer() #creates new recognizer object
+
+#dictionary = [("fenn hall", 0.4), ("testing", 0.6)]
 
 #threshold volume to start recording
 recognizer.energy_threshold = 9050
@@ -16,9 +19,12 @@ recognizer.operation_timeout = 5 #sets API timeout to 5 sec
 fenn = [("Fenn Hall", 0.6)]
 
 with sr.Microphone() as source:
-    recognizer.adjust_for_ambient_noise(source)
+#    recognizer.adjust_for_ambient_noise(source)
     audio = recognizer.listen(source, timeout=5)
+    print("Heard something.")
     transcript = recognizer.recognize_sphinx(audio,language='en-US',
                                              keyword_entries=None,
                                              show_all=False)
+    #for best, i in zip(transcript.nbest(), range(100)):
+    #    print(best.hypstr, best.score)
     print(transcript)
