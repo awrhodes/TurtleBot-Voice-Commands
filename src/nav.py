a1c35970bdbf3ef7231d9a96bc3ab2ed12bd9232
Alex <Alex Rhodes>
2017-05-01 17:37:31 -0400

    Fixed syntax

--- src/nav.py
+++ src/nav.py
@@ -10,7 +10,6 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from Dixon.msg import command
 from nav_msgs.msg import Odometry
 
-action_flag = 0  # 0: waiting, 1: moving locally, 2: moving globally
 
 class Nav:
     def __init__(self):
@@ -18,53 +17,56 @@ class Nav:
         rospy.loginfo("Nav module running ...")
 
         # init var (for move commands only)
-        self.lin_speed = 0.5 
+        self.lin_speed = 0.5
         self.lin_dir = 0
-        
+
         # current global position
         self.current_x = None
         self.current_y = None
-        
+
+        # 0: waiting, 1: moving locally, 2: moving globally
+        self.action_flag = 0
+
         threadObj = threading.Thread(target=self.publishLocal)
         threadObj.start()
         # subscriber
         rospy.Subscriber('Dixon/command', command, self.moveCallback)
-        rospy.Subscriber('odom', Odometry, self.odomCallback)  
+        rospy.Subscriber('odom', Odometry, self.odomCallback)
 
     def moveCallback(self, msg):
         # check the command
         # this program structure is temp
         # will be changed when nav goals are added
         # will use different functions for different commands
-        if msg.local == True
-            if msg.command == "go":
-                action_flag = 1
+        if msg.local is True:
+            if msg.command is "go":
+                self.action_flag = 1
                 if msg.destination == "forward":
                     self.lin_dir = 1
                 elif msg.destination == "backward":
                     self.lin_dir = -1
             elif msg.command == "stop":
-                action_flag = 0
+                self.action_flag = 0
                 self.lin_dir = 0
         else:
             if msg.command == "go":
                 # actionlib send goal
-                action_flag = 2
+                self.action_flag = 2
                 self.moveToGoal(msg.x, msg.y)
             elif msg.command == "stop":
                 # actionlib send stop goal
-                action_flag = 0
+                self.action_flag = 0
                 self.moveToGoal(self.current_x, self.current.y)
 
         print(msg.command)
         print(self.lin_dir)
 
-        #vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        # vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
     def odomCallback(self, msg):
         self.current_x = msg.pose.pose.position.x
         self.current_y = msg.pose.pose.position.y
-       
+
     def publishLocal(self):
         vel_pub = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=10)
         rate = rospy.Rate(100)    # 100 Hz
@@ -87,12 +89,12 @@ class Nav:
             rospt.loginfo("Waiting for move_base action server to respond ...")
             goal = MoveBaseGoal()
 
-            # header 
+            # header
             goal.target_pose.header.frame_id = "map"
             goal.target_pose.header.stamp = rospy.Time.now()
 
             # point and orientation
-            goal.target_pose.pose.position = Point(x,y,0)
+            goal.target_pose.pose.position = Point(x, y, 0)
             goal.target_pose.pose.orientation.x = 0.0
             goal.target_pose.pose.orientation.y = 0.0
             goal.target_pose.pose.orientation.z = 0.0
